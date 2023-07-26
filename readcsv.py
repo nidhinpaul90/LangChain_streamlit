1529a23b0d981ea0f5ecf69e9865800722abf778
nidhinpaul90 <34165889+nidhinpaul90@users.noreply.github.com>
2023-07-26 15:28:03 +0530

    Create readcsv.py

---
+++ readcsv.py
@@ -0,0 +1,26 @@
+from langchain.agents import create_csv_agent
+import os
+import streamlit as st
+from langchain.llms import Cohere
+
+
+def main():
+
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV 📈")
+
+    csv_file = st.file_uploader("Upload a CSV file", type="csv")
+    if csv_file is not None:
+
+        agent = create_csv_agent(
+            Cohere(cohere_api_key="YOUR_API_KEY_HERE"), csv_file , verbose=True)
+
+        user_question = st.text_input("Ask a question about your CSV: ")
+
+        if user_question is not None and user_question != "":
+            with st.spinner(text="In progress..."):
+                st.write(agent.run(user_question))
+
+
+if __name__ == "__main__":
+    main()
